2a570db0ec256e3af2a8dab8756107af1372f2e8
Maksym-Kovorotnyi <Kovorotny1@gmail.com>
2023-05-26 12:35:26 +0300

    create app and server files

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const logger = require("logger");
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
