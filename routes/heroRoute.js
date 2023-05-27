03686f36dd5ad607a467365cf9b4129b3dc5e5ed
Maksym-Kovorotnyi <Kovorotny1@gmail.com>
2023-05-27 13:47:41 +0300

    change app main route

--- routes/heroRoute.js
+++ routes/heroRoute.js
@@ -4,20 +4,20 @@ const ctrl = require("../controllers/hero");
 const { validateBody, isvalidId, handleUpload } = require("../middlewares");
 const { addSchema } = require("../models/hero");
 
-router.get("/heroes", ctrl.getAllHeroes);
+router.get("/", ctrl.getAllHeroes);
 
-router.get("/heroes/:id", isvalidId, ctrl.findHeroById);
+router.get("/:id", isvalidId, ctrl.findHeroById);
 
 router.post("/", validateBody(addSchema), handleUpload, ctrl.addHero);
 
 router.patch(
-  "/heroes/:id",
+  "/:id",
   isvalidId,
   validateBody(addSchema),
   handleUpload,
   ctrl.changeHero
 );
 
-router.delete("/heroes/:id", isvalidId, ctrl.deleteHero);
+router.delete("/:id", isvalidId, ctrl.deleteHero);
 
 module.exports = router;
