46f325f6041b1aba8f1b10daaa1c5d6f39cc5be0
Maksym-Kovorotnyi <Kovorotny1@gmail.com>
2023-05-28 14:17:38 +0300

    Update uploadImages.js

--- middlewares/uploadImages.js
+++ middlewares/uploadImages.js
@@ -3,6 +3,8 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
 const { CLOUD_NAME, API_KEY, SECRET_KEY } = process.env;
 
+const ALLOWED_FORMATS = ["jpg", "png", "svg", "webp"];
+
 cloudinary.config({
   cloud_name: CLOUD_NAME,
   api_key: API_KEY,
@@ -13,11 +15,25 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: "heroes",
+    allowedFormats: ALLOWED_FORMATS,
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
   },
 });
 
 const multerUploads = multer({
   storage,
+  limits: 3145728,
+  fileFilter: (req, file, cb) => {
+    const allowedFormats = ALLOWED_FORMATS;
+    const fileExtension =
+      file.originalname.split(".")[file.originalname.split(".").length - 1];
+    if (!allowedFormats.includes(fileExtension)) {
+      return cb(new Error("Invalid file format"));
+    }
+    cb(null, true);
+  },
 }).single("images");
 
 const handleUpload = (req, res, next) => {
